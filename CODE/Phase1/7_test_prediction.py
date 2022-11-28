8bf2ecf2d8052e149dd4f0577207f38839dcb6f3
Akanksha Singh <akankshasingh25march@gmail.com>
2022-11-28 11:42:55 +0530

    Update 7_test_prediction.py

--- CODE/Phase1/7_test_prediction.py
+++ CODE/Phase1/7_test_prediction.py
@@ -37,7 +37,7 @@ pipe = Pipeline(
             ("vect", CountVectorizer(min_df=5, max_df=0.7, ngram_range = (1,3))),
             #("vect", TfidfVectorizer(min_df=5, max_df=0.7)),
             ("selector", SelectKBest(score_func=chi2, k=10)),
-            ("clf", LogisticRegression(max_iter= 1000)),
+            ("clf", LogisticRegression(max_iter= 100)),
             #("clf", KNeighborsClassifier(metric = 'manhattan', n_neighbors = 5)),
             #("clf", RandomForestClassifier(class_weight='balanced')),
             #("clf", MultinomialNB(alpha= 16.666733333333333, fit_prior = False)),
